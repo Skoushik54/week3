0c6e72c7f98ea2d89510c4d47e3308e05c051b9c
Skoushik54 <140392717+Skoushik54@users.noreply.github.com>
2024-11-23 11:45:06 +0530

    added addition of 2 numbers

--- prime.java
+++ prime.java
@@ -1,5 +1,9 @@
 class prime{
     public static void main(String[] args) {
         System.out.println("Hello World");
+        int a,b;
+        a=10;
+        b=10;
+        System.err.println(a+b);
     }
 }
